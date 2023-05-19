b6c773e9846271133850bce97f721a484b2dbd07
ChahrazadBouragba <chbouragba25@gmail.com>
2023-05-19 02:20:03 +0100

    upp button updated

--- script.js
+++ script.js
@@ -24,3 +24,39 @@ nextButton.addEventListener('click', () => {
     behavior: 'smooth'
   });
 });
+
+
+
+//! UP ARROW
+// Create the arrow element
+// const arrow = document.createElement('a');
+// arrow.href = '#section1';
+// arrow.className = 'upArrow';
+// arrow.innerHTML = '<img class="upArrow" src="/img/up-arrow.png">';
+
+// // Get all the sections on the page
+// const sections = document.querySelectorAll('section');
+
+// // Append the arrow element to each section
+// sections.forEach(section => {
+//   const clonedArrow = arrow.cloneNode(true);
+//   section.appendChild(clonedArrow);
+// });
+
+window.onscroll = function () {
+  toggleScrollUpButton();
+};
+
+function toggleScrollUpButton() {
+  const scrollUpButton = document.querySelector(".upArrow");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollUpButton.style.display = "block";
+  } else {
+    scrollUpButton.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
